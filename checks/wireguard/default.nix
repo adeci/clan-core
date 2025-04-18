b4825a3da9c204fceb1ab977dcc21a7337e77788
pinpox <clan@pablo.tools>
2025-04-18 17:50:09 +0200

    Add test

--- checks/wireguard/default.nix
+++ checks/wireguard/default.nix
@@ -19,11 +19,10 @@ clanLib.test.makeTestClan {
     {
       name = "wireguard";
 
-
       clan = {
         directory = ./.;
         inventory = {
-      modules."@clan/wireguard" = import ../../clanServices/wireguard/default.nix;
+          modules."@clan/wireguard" = import ../../clanServices/wireguard/default.nix;
 
           machines = lib.genAttrs machines (_: { });
 
@@ -35,18 +34,15 @@ clanLib.test.makeTestClan {
 
               roles.controller.machines."controller".settings = {
                 ip = "192.168.8.1";
-                # endpoint = "vpn.pablo.tools:51820";
+                endpoint = "192.168.1.1:51920";
               };
 
               roles.peer.machines = {
-                peer1.settings.ip = "192.168.8.1";
-                peer2.settings.ip = "192.168.8.2";
-                # peer2.settings.extraIPs = [ "192.168.2.0/24" ];
+                peer1.settings.ip = "192.168.8.2";
+                peer2.settings.ip = "192.168.8.3";
               };
-
             };
           };
-
         };
       };
 
@@ -77,15 +73,42 @@ clanLib.test.makeTestClan {
         # admin.clan.data-mesher.network.tld = "foo";
       };
 
-      # TODO Add better test script.
       testScript = ''
-
         start_all()
 
-        controller.wait_for_unit("wireguard")
-        peer1.wait_for_unit("wireguard")
-        peer2.wait_for_unit("wireguard")
+        controller.systemctl("start network-online.target")
+        controller.wait_for_unit("network-online.target")
+        peer1.systemctl("start network-online.target")
+        peer1.wait_for_unit("network-online.target")
+        peer2.systemctl("start network-online.target")
+        peer2.wait_for_unit("network-online.target")
 
+        # Show all addresses
+        controller.succeed("ip a >&2")
+        peer1.succeed("ip a show wg-test >&2")
+        peer2.succeed("ip a show wg-test >&2")
+
+        controller.succeed("wg >&2")
+        peer1.succeed("wg >&2")
+        peer2.succeed("wg >&2")
+
+        # Ping from controller
+        controller.succeed("ping -c5 192.168.8.1")
+        controller.succeed("ping -c5 192.168.8.2")
+        controller.succeed("ping -c5 192.168.8.3")
+
+        # Ping from peer to contoller
+        peer1.succeed("ping -c5 192.168.8.1")
+        # peer1.succeed("ping -c5 fc00::1")
+
+        # Ping from peer to peer
+        peer2.succeed("ping -c5 192.168.8.2")
+        # peer2.succeed("ping -c5 fc00::1")
+
+        with subtest("Has PSK set"):
+          peer1.succeed("wg | grep 'preshared key'")
+          peer2.succeed("wg | grep 'preshared key'")
+          controller.succeed("wg | grep 'preshared key'")
       '';
     }
   );
