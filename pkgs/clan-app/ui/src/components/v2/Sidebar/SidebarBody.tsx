3b1186ba3a25ae490859ad624ab53f7e492fafeb
Brian McGee <brian@bmcgee.ie>
2025-06-13 14:30:08 +0100

    wip

--- pkgs/clan-app/ui/src/components/v2/Sidebar/SidebarBody.tsx
+++ pkgs/clan-app/ui/src/components/v2/Sidebar/SidebarBody.tsx
@@ -1,5 +1,7 @@
 import "./SidebarBody.css"
 import { A } from "@solidjs/router";
+import { Accordion } from "@kobalte/core/accordion";
+import Icon from "../Icon/Icon";
 
 export interface SidebarBodyProps {
 
@@ -9,7 +11,38 @@ export const SidebarBody = (props: SidebarBodyProps) => {
 
   return (
     <div class="sidebar-body">
-      <A href="/foo">Home</A>
+      <nav>
+        <Accordion class="accordion" multiple>
+          <Accordion.Item class="item" value="foo">
+            <Accordion.Header class="header">
+              <Accordion.Trigger class="trigger">
+                <span>Foo</span>
+                <Icon icon="CaretDown" inverted={true}/>
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content class="content">
+              <A href="/foo">Foo</A>
+              <A href="/foo">Bar</A>
+              <A href="/foo">baz</A>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item class="item" value="bar">
+            <Accordion.Header class="header">
+              <Accordion.Trigger class="trigger">
+                <span>Foo</span>
+                <Icon icon="CaretDown" inverted={true}/>
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content class="content">
+              <A href="/foo">Foo</A>
+              <A href="/foo">Bar</A>
+              <A href="/foo">baz</A>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+
+      </nav>
+
     </div>
   )
 }
